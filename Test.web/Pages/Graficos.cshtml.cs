3d3cfde8e24fa469a2e9fefbf19ff701c9d31c86
cgonzalez26 <crisgonzalez26@gmail.com>
2024-10-04 13:47:39 -0300

    Agregar graficos con chartjs

---
+++ Test.web/Pages/Graficos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test.web.Pages
+{
+    public class GraficosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
